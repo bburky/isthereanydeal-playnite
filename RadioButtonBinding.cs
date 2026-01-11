88dbdec95cb48b82d64f6c9e86f05adc6ecb2839
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2026-01-11 21:32:06 +0800

    Refactor BoolInverterConverter:
    Remove redundant `using`s.
    
    Add ValueConversion as per recommendation: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/data/how-to-convert-bound-data
    
    Remove checking. It's a developer mistake to use these in non-boolean context.

--- RadioButtonBinding.cs
+++ RadioButtonBinding.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
-using System.Windows.Markup;
 
 namespace IsthereanydealCollectionSync
 {
+    [ValueConversion(typeof(bool), typeof(bool))]
     // https://stackoverflow.com/a/3361553
     public class BoolInverterConverter : IValueConverter
     {
@@ -17,21 +12,13 @@ namespace IsthereanydealCollectionSync
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            if (value is bool)
-            {
-                return !(bool)value;
-            }
-            return value;
+            return !(bool)value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            if (value is bool)
-            {
-                return !(bool)value;
-            }
-            return value;
+             return !(bool)value;
         }
 
         #endregion
